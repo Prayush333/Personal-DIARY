d33fea38a52e302aedf23239bd6e2c996f0006d4
Prayush Neupane <pyprayushneupane@gmail.com>
2025-06-03 17:40:26 +0545

    added note_list.html+add_note.html+edit_note.html+delete_note.html+base.html

--- diary_app/views.py
+++ diary_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Note
 
-# Create your views here.
+
